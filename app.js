40908a36975c18034733c261dd7d035117375db3
unknown <sanjaych9517@gmail.com>
2026-08-24 17:24:57 +0530

    we add index route in our projects

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 const Item = require("./models/item.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Ecommerce";
 const PORT = 5001;
@@ -12,6 +13,9 @@ main().then(() =>{
    console.log(err);
 });
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 async function main() {
     await mongoose.connect(MONGO_URL);
 }
@@ -21,19 +25,27 @@ app.get("/",(req, res) =>{
     res.send("Hi I am a ROOT");
 });
 
-app.get("/textItem", async (req, res) =>{
-    let sampleItem = new Item({
-        title: "My New Product",
-        description: "new phone",
-        price: 18999,
-        category:"Mobile Phones",
-        brand:"OPPO",
-        stock: 10,
-    });
-   await sampleItem.save();
-   console.log("sample was saved");
-   res.send("successfully testing");
-})
+// index route start
+app.get("/items", async (req, res) => {
+     const allItems = await Item.find({});
+     res.render("items/index.ejs", {allItems});
+});
+ // index route end
+
+// app.get("/textItem", async (req, res) =>{
+//     let sampleItem = new Item({
+//         title: "My New Product",
+//         description: "new phone",
+//         price: 18999,
+//         category:"Mobile Phones",
+//         brand:"OPPO",
+//         stock: 10,
+//     });
+
+//    await sampleItem.save();
+//    console.log("sample was saved");
+//    res.send("successfully testing");
+// });
 
 app.listen(PORT, () => {
   console.log(`server is listening on PORT http://localhost:${PORT}`)
