0599cf3b326dd6b3546d4b1035f2c42a6522db48
unknown <sanjaych9517@gmail.com>
2026-08-24 15:31:34 +0530

    we successfully push data in our database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+const Item = require("./models/item.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Ecommerce";
 const PORT = 5001;
@@ -19,6 +20,20 @@ async function main() {
 app.get("/",(req, res) =>{
     res.send("Hi I am a ROOT");
 });
+
+app.get("/textItem", async (req, res) =>{
+    let sampleItem = new Item({
+        title: "My New Product",
+        description: "new phone",
+        price: 18999,
+        category:"Mobile Phones",
+        brand:"OPPO",
+        stock: 10,
+    });
+   await sampleItem.save();
+   console.log("sample was saved");
+   res.send("successfully testing");
+})
 
 app.listen(PORT, () => {
   console.log(`server is listening on PORT http://localhost:${PORT}`)
---
+++ models/item.js
@@ -0,0 +1,24 @@
+const mongoose= require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const itemScheme = new Schema({
+    title: {
+       type: String,
+       required: true,
+    },
+    description: String,
+    image: {
+        default: "https://m.media-amazon.com/images/I/71TjL3zBQQL._SX679_.jpg",
+        type:String,
+        set: (v) => v ==="" ? "https://m.media-amazon.com/images/I/71TjL3zBQQL._SX679_.jpg" : v,
+    },
+    price: Number,
+    category: String,
+    brand: String,
+    stock: Number,
+});
+
+const Item = mongoose.model("Item",itemScheme);
+
+module.exports = Item;
